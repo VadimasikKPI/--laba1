abcea28e6ee1ec55fd0b42e61adc07e5c08c3fdd
Кобельський Вадим <kobelskiy73@fmail.com>
2021-09-05 15:55:41 +0300

    add window, caption, background fill and while loop of game

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import pygame as pg
 
 pg.init()
+window = pg.display.set_mode((700, 600))
+pg.display.set_caption("Space Invaders")
+
+
+isClose = True
+while isClose:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            isClose = False
+
+    window.fill((0, 0, 0))
+    pg.display.update()
