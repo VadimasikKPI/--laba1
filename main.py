8b67c8ceb6f69446a0c7faed7e90ef716128d3f1
Кобельський Вадим <kobelskiy73@fmail.com>
2021-09-05 22:57:53 +0300

    add score

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame as pg
+import math
+
 #
 pg.init()
 window = pg.display.set_mode((800, 600))
@@ -12,8 +14,18 @@ playerIcon = pg.image.load('player.png')
 playerXcoord = 360
 playerYcoord = 470
 playerSpeedChange = 0
+
+score = 0
+font = pg.font.Font('freesansbold.ttf', 30)
+
+def showScore(scoreXcoord, scoreYcoord):
+    totalScore = font.render("Score: " + str(score), True, (255, 255, 255))
+    window.blit(totalScore, (scoreXcoord, scoreYcoord))
+
+
 def player(playerX, playerY):
     window.blit(playerIcon, (playerX, playerY))
+
 
 #
 enemyIcon = pg.image.load('enemy1.png')
@@ -21,8 +33,11 @@ enemyXcoord = 360
 enemyYcoord = 50
 enemySpeedChange = 0.15
 enemyHightChange = 40
+
+
 def enemy(enemyX, enemyY):
     window.blit(enemyIcon, (enemyX, enemyY))
+
 
 #
 bulletIcon = pg.image.load('star_bullet.png')
@@ -30,10 +45,21 @@ bulletXcoord = 360
 bulletYcoord = 470
 bulletHightChange = 0.45
 bulletIsReady = "Ready"
+
+
 def bullet(bulletX, bulletY):
     global bulletIsReady
     bulletIsReady = "Not ready"
     window.blit(bulletIcon, (bulletX + 32, bulletY + 10))
+
+
+def collision(enemyXcoord, enemyYcoord, bulletXcoord, bulletYcoord):
+    isCollision = math.sqrt((math.pow(enemyXcoord - bulletXcoord, 2)) + (math.pow(enemyYcoord - bulletYcoord, 2)))
+    if isCollision < 27:
+        return True
+    else:
+        return False
+
 
 #
 isClose = True
@@ -78,11 +104,18 @@ while isClose:
     if bulletIsReady == "Not ready":
         bullet(bulletXcoord, bulletYcoord)
         bulletYcoord -= bulletHightChange
-    if bulletYcoord <=0:
+    if bulletYcoord <= 0:
         bulletIsReady = "Ready"
         bulletYcoord = 470
 
+    isCollision = collision(enemyXcoord, enemyYcoord, bulletXcoord, bulletYcoord)
+    if isCollision:
+        bulletYcoord = 470
+        bulletIsReady = "Ready"
+        score += 10
+
     #
     player(playerXcoord, playerYcoord)
+    showScore(10, 10)
     enemy(enemyXcoord, enemyYcoord)
     pg.display.update()
