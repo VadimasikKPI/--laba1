817c9a27605ac60ca1921ab63ba1c53823173ddc
Кобельський Вадим <kobelskiy73@fmail.com>
2021-09-05 16:36:01 +0300

    add player move on X coord and speed

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ pg.display.set_icon(icon)
 
 
 playerIcon = pg.image.load('player.png')
-
+playerXcoord = 360
+playerYcoord = 470
+playerSpeedChange = 0
 def player(playerX, playerY):
     window.blit(playerIcon, (playerX, playerY))
 
@@ -19,5 +21,16 @@ while isClose:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             isClose = False
-    player(360, 400)
+
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_RIGHT or event.key == pg.K_d:
+                playerSpeedChange = 0.5
+            if event.key == pg.K_LEFT or event.key == pg.K_a:
+                playerSpeedChange = -0.5
+        if event.type == pg.KEYUP:
+            if event.key == pg.K_LEFT or event.key == pg.K_RIGHT or event.key == pg.K_d or event.key == pg.K_a:
+                playerSpeedChange = 0
+
+    playerXcoord += playerSpeedChange
+    player(playerXcoord, playerYcoord)
     pg.display.update()
