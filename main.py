ab73b683cc23e567a26c2b34516315bbe2f91db0
Кобельський Вадим <kobelskiy73@fmail.com>
2021-09-05 22:16:30 +0300

    add shooting and rebalance multiple shooting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame as pg
-
+#
 pg.init()
 window = pg.display.set_mode((800, 600))
 pg.display.set_caption("Space Invaders")
@@ -7,6 +7,7 @@ icon = pg.image.load('Caption.png')
 pg.display.set_icon(icon)
 background = pg.image.load('space.png')
 
+#
 playerIcon = pg.image.load('player.png')
 playerXcoord = 360
 playerYcoord = 470
@@ -14,14 +15,27 @@ playerSpeedChange = 0
 def player(playerX, playerY):
     window.blit(playerIcon, (playerX, playerY))
 
+#
 enemyIcon = pg.image.load('enemy1.png')
 enemyXcoord = 360
 enemyYcoord = 50
-enemySpeedChange = 0.25
+enemySpeedChange = 0.15
 enemyHightChange = 40
 def enemy(enemyX, enemyY):
     window.blit(enemyIcon, (enemyX, enemyY))
 
+#
+bulletIcon = pg.image.load('star_bullet.png')
+bulletXcoord = 360
+bulletYcoord = 470
+bulletHightChange = 0.45
+bulletIsReady = "Ready"
+def bullet(bulletX, bulletY):
+    global bulletIsReady
+    bulletIsReady = "Not ready"
+    window.blit(bulletIcon, (bulletX + 32, bulletY + 10))
+
+#
 isClose = True
 while isClose:
     window.fill((0, 0, 0))
@@ -35,23 +49,40 @@ while isClose:
                 playerSpeedChange = 0.5
             if event.key == pg.K_LEFT or event.key == pg.K_a:
                 playerSpeedChange = -0.5
+            if event.key == pg.K_SPACE:
+                if bulletIsReady == "Ready":
+                    bulletXcoord = playerXcoord
+                    bullet(bulletXcoord, bulletYcoord)
+                    bulletYcoord -= bulletHightChange
         if event.type == pg.KEYUP:
             if event.key == pg.K_LEFT or event.key == pg.K_RIGHT or event.key == pg.K_d or event.key == pg.K_a:
                 playerSpeedChange = 0
 
+    #
     playerXcoord += playerSpeedChange
     if playerXcoord <= 0:
         playerXcoord = 0
     elif playerXcoord >= 740:
         playerXcoord = 740
 
+    #
     enemyXcoord += enemySpeedChange
     if enemyXcoord <= 0:
-        enemySpeedChange = 0.25
+        enemySpeedChange = 0.15
         enemyYcoord += enemyHightChange
     elif enemyXcoord >= 740:
-        enemySpeedChange = -0.25
+        enemySpeedChange = -0.15
         enemyYcoord += enemyHightChange
+
+    #
+    if bulletIsReady == "Not ready":
+        bullet(bulletXcoord, bulletYcoord)
+        bulletYcoord -= bulletHightChange
+    if bulletYcoord <=0:
+        bulletIsReady = "Ready"
+        bulletYcoord = 470
+
+    #
     player(playerXcoord, playerYcoord)
     enemy(enemyXcoord, enemyYcoord)
     pg.display.update()
