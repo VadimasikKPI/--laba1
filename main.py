9ae70a83bfbc2f094fc895e08d91e7c46930e2a3
Кобельський Вадим <kobelskiy73@fmail.com>
2021-09-05 17:21:00 +0300

    add background image

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ window = pg.display.set_mode((800, 600))
 pg.display.set_caption("Space Invaders")
 icon = pg.image.load('Caption.png')
 pg.display.set_icon(icon)
-
+background = pg.image.load('space.png')
 
 playerIcon = pg.image.load('player.png')
 playerXcoord = 360
@@ -25,6 +25,7 @@ def enemy(enemyX, enemyY):
 isClose = True
 while isClose:
     window.fill((0, 0, 0))
+    window.blit(background, (0, 0))
     for event in pg.event.get():
         if event.type == pg.QUIT:
             isClose = False
@@ -47,8 +48,10 @@ while isClose:
     enemyXcoord += enemySpeedChange
     if enemyXcoord <= 0:
         enemySpeedChange = 0.25
+        enemyYcoord += enemyHightChange
     elif enemyXcoord >= 740:
         enemySpeedChange = -0.25
+        enemyYcoord += enemyHightChange
     player(playerXcoord, playerYcoord)
     enemy(enemyXcoord, enemyYcoord)
     pg.display.update()
