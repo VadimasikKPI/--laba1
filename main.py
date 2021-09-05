f755e27160064ba2fad56734bc4b6b73161c56a4
Кобельський Вадим <kobelskiy73@fmail.com>
2021-09-05 16:13:18 +0300

    add player drawing function1.0

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ import pygame as pg
 pg.init()
 window = pg.display.set_mode((700, 600))
 pg.display.set_caption("Space Invaders")
-icon = pg.image.load('')
+icon = pg.image.load('Caption.png')
 pg.display.set_icon(icon)
+
+
+playerIcon = pg.image.load('player.png')
+
+def player(playerX, playerY):
+    window.blit(playerIcon, (playerX, playerY))
+
 
 isClose = True
 while isClose:
+    window.fill((0, 0, 0))
     for event in pg.event.get():
         if event.type == pg.QUIT:
             isClose = False
-
-    window.fill((0, 0, 0))
+    player(360, 400)
     pg.display.update()
