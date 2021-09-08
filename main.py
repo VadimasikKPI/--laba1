6014438da9380b9757496746fa0ae7204cd1dd8b
Кобельський Вадим <kobelskiy73@fmail.com>
2021-09-08 19:20:20 +0300

    add comments and rebuild game_lose if

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame as pg
 import math
 import random
 
-#
+#Ініціюю pygame, створюю вікно гри, назначаю заголовок і бекграунд
 pg.init()
 window = pg.display.set_mode((800, 600))
 pg.display.set_caption("Space Invaders")
@@ -10,28 +10,29 @@ icon = pg.image.load('Caption.png')
 pg.display.set_icon(icon)
 background = pg.image.load('space2.png')
 
-#
+#Створюю гравця, його координати
 playerIcon = pg.image.load('player.png')
 playerXcoord = 360
 playerYcoord = 470
 playerSpeedChange = 0
 
+#Рахунок, шрифти і лічильник для кількості ворогів
 score = 0
 font = pg.font.Font('freesansbold.ttf', 30)
 gameOver = pg.font.Font('freesansbold.ttf', 50)
-counter = 0
+counter_enemy = 0
 
-
+#Функція яка виводить рахунок на екран
 def showScore(scoreXcoord, scoreYcoord):
     totalScore = font.render("Score: " + str(score), True, (255, 255, 255))
     window.blit(totalScore, (scoreXcoord, scoreYcoord))
 
-
+#Функція яка виводить гравця на екран
 def player(playerX, playerY):
     window.blit(playerIcon, (playerX, playerY))
 
 
-#
+#СТворення списків для масива ворогів
 enemyIcon = []
 enemyXcoord = []
 enemyYcoord = []
@@ -46,27 +47,27 @@ for i in range(numOfEnemies):
     enemyXcoord.append(random.randint(0, 800))
     enemyYcoord.append(random.randint(50, 100))
     enemySpeedChange.append(0.15)
-    enemyHightChange.append(40)
+    enemyHightChange.append(150)
 
-
+#Функція яка виводить ворогів на екран
 def enemy(enemyX, enemyY, i):
     window.blit(enemyIcon[i], (enemyX, enemyY))
 
 
-#
+#Ініціалізація патрону(зброї)
 bulletIcon = pg.image.load('star_bullet.png')
 bulletXcoord = 360
 bulletYcoord = 470
 bulletHightChange = 0.45
 bulletIsReady = "Ready"
 
-
+#Функція яка виводить пулю на екран та перевіряє її готовність
 def bullet(bulletX, bulletY):
     global bulletIsReady
     bulletIsReady = "Not ready"
     window.blit(bulletIcon, (bulletX + 32, bulletY + 10))
 
-
+#Функція яка перевіряє чи доторкались вороги та пулі
 def collision(enemyXcoord, enemyYcoord, bulletXcoord, bulletYcoord):
     isCollision = math.sqrt((math.pow(enemyXcoord - bulletXcoord, 2)) + (math.pow(enemyYcoord - bulletYcoord, 2)))
     if isCollision < 30:
@@ -74,17 +75,19 @@ def collision(enemyXcoord, enemyYcoord, bulletXcoord, bulletYcoord):
     else:
         return False
 
-
+#Функція яка виводить привітання на екран
 def end_game_win():
     gameOverTitle = gameOver.render("YOU WIN! CONGRATULATIONS", True, (255, 255, 255))
     window.blit(gameOverTitle, (10, 250))
 
+#Функція яка виводить повідомлення про програш на екран
 def end_game_lose():
-    gameOverTitle = gameOver.render("YOU LOSE! TRY NEXT TIME!", True, (255, 255, 255))
-    window.blit(gameOverTitle, (50, 250))
+    gameOvertitle = gameOver.render("YOU LOSE! TRY NEXT TIME!", True, (255, 255, 255))
+    window.blit(gameOvertitle, (50, 250))
 
 
-#
+
+#Цикл в якому запускається і працює вся гра
 isClose = True
 while isClose:
     window.fill((0, 0, 0))
@@ -130,14 +133,14 @@ while isClose:
             bulletIsReady = "Ready"
             score += 10
             enemyYcoord[i] = 2000
-            counter += 1
+            counter_enemy += 1
         enemy(enemyXcoord[i], enemyYcoord[i], i)
-        if counter == numOfEnemies:
+        if counter_enemy == numOfEnemies:
             end_game_win()
             break
         if enemyYcoord[i]>400 and enemyYcoord[i]<1900 :
             for j in range(numOfEnemies):
-                enemyYcoord[j] = 2000
+                enemyYcoord[j] = 1500
             end_game_lose()
             break
 
